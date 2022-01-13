65a09210c2db2ec1fd848105d6ad7e03057d1a55
Oleksandr Baglai <oleksandr_baglai@epam.com>
2022-01-13 12:42:10 +0200

    [all] Improve performance. Replace Mockito.mock(Dice.class) -> new MockDice().

--- src/test/java/com/codenjoy/dojo/rubicscube/model/RandomCommandTest.java
+++ src/test/java/com/codenjoy/dojo/rubicscube/model/RandomCommandTest.java
@@ -23,23 +23,23 @@ package com.codenjoy.dojo.rubicscube.model;
  */
 
 
-import com.codenjoy.dojo.services.Dice;
+import com.codenjoy.dojo.services.dice.MockDice;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class RandomCommandTest {
 
     @Test
     public void shouldGenerate() {
         // given
-        Dice dice = mock(Dice.class);
+        MockDice dice = new MockDice();
         RandomCommand generator = new RandomCommand(dice);
 
-        when(dice.next(100)).thenReturn(6*3);
-        when(dice.next(6*3)).thenReturn(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
+        dice.whenThen(100,
+                6*3);
+        dice.whenThen(6*3,
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17);
 
         //when
         String next = generator.next();
